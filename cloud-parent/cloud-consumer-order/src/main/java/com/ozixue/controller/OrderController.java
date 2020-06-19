f56b2710870a3c69f3475a3970186e9773e0752c
lirubao <lirubaogo@gmail.com>
2020-06-20 03:21:53 +0800

    服务总线整合:bus + rabbitMQ

--- cloud-parent/cloud-consumer-order/src/main/java/com/ozixue/controller/OrderController.java
+++ cloud-parent/cloud-consumer-order/src/main/java/com/ozixue/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.ozixue.entity.Payment;
 import com.ozixue.service.PaymentFeignService;
 import com.ozixue.vo.JsonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +16,23 @@ import javax.annotation.Resource;
 @Slf4j
 @RestController
 @RequestMapping("/consumer")
+@RefreshScope
 public class OrderController {
 
     @Resource
     private PaymentFeignService paymentFeignService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort:" + serverPort + "\t\n\n configInfo: " + configInfo;
+    }
 
 
     @GetMapping("/payment/find/{id}")

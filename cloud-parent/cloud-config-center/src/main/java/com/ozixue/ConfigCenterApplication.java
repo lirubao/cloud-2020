8939a0a8be525f6c060b33c81a971660833662fa
lirubao <lirubaogo@gmail.com>
2020-06-20 01:25:31 +0800

    配置中心test

---
+++ cloud-parent/cloud-config-center/src/main/java/com/ozixue/ConfigCenterApplication.java
@@ -0,0 +1,16 @@
+package com.ozixue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApplication.class, args);
+    }
+}
